3f31d60fe8ff305b7a57bcfeaa2cc9b14ad58ba4
Przemysław Szelenberger <p.szelenberger@gmail.com>
2016-02-05 00:10:34 +0100

    installed packages css & text

--- src/config.js
+++ src/config.js
@@ -17,6 +17,9 @@ System.config({
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "core-js": "npm:core-js@1.2.6",
+    "css": "github:systemjs/plugin-css@0.1.20",
+    "json": "github:systemjs/plugin-json@0.1.0",
+    "text": "github:systemjs/plugin-text@0.0.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
